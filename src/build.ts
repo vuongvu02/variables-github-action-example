71d1a9b78f5ea7d8c7970e49918380802b2f1294
Hoang Vuong Vu <vu@bikeleasing.com>
2025-04-23 00:38:14 +0200

    feat: add auto-generated CSS and SCSS variable files; implement CSS import file generation

--- src/build.ts
+++ src/build.ts
@@ -1,4 +1,5 @@
 import * as glob from 'glob';
+import * as fs from 'fs';
 import { Config } from 'style-dictionary';
 import { StyleDictionary } from 'style-dictionary-utils';
 import { TokenSourceWithMode } from './types.js';
@@ -14,7 +15,7 @@ const PRIMITIVE_SET_NAME = process.env.PRIMITIVE_SET_NAME || 'primitives';
 
 const inputTokenSets = glob.sync('tokens/**/*.json');
 
-export const getModeFromFilePath = (path: string): string => {
+const getModeFromFilePath = (path: string): string => {
   const modeMatch = path.match(/tokens\/[^.]+\.([^.]+)\.json/) || [];
   return modeMatch[1].toLowerCase();
 };
@@ -68,7 +69,7 @@ const getConfigs = (): Config[] => {
     getDefaultTokenSet(),
     ...getTokenSets(THEME_MODES),
     ...getTokenSets(RESPONSIVE_MODES),
-  ].map(({ mode = 'default', tokenSource }) => ({
+  ].map(({ mode = 'base', tokenSource }) => ({
     source: tokenSource,
     platforms: {
       css: {
@@ -119,11 +120,36 @@ const getConfigs = (): Config[] => {
   }));
 };
 
+const generateCSSImportFile = (outputCSSRefs: string[]) => {
+  const buildCssPath = 'build/css';
+  const cssFilePath = `${buildCssPath}/variables.css`;
+
+  if (!fs.existsSync(buildCssPath)) {
+    fs.mkdirSync(buildCssPath, { recursive: true });
+  } else if (fs.existsSync(cssFilePath) && fs.statSync(cssFilePath).isDirectory()) {
+    fs.rmSync(cssFilePath, { recursive: true });
+  }
+
+  const cssImports = outputCSSRefs
+    .filter(Boolean)
+    .map((ref) => `@import url('./${ref}');`)
+    .join('\n');
+
+  fs.writeFileSync(
+    cssFilePath,
+    `/**\n* Do not edit directly, this file was auto-generated.\n*/\n\n${cssImports}\n`,
+  );
+
+  console.info('Generated CSS import file:', cssFilePath);
+};
+
 async function run() {
   const configs = getConfigs();
+  const outputCSSRefs: string[] = [];
 
   configs.forEach(async (config, index) => {
     const sd = new StyleDictionary(config);
+    outputCSSRefs.push(config.platforms?.css.files?.[0].destination || '');
 
     // only clean the build directory on the first run
     if (index === 0) {
@@ -131,6 +157,8 @@ async function run() {
     }
     await sd.buildAllPlatforms();
   });
+
+  generateCSSImportFile(outputCSSRefs);
 }
 
 run();
